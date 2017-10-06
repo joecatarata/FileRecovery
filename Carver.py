dfd156578ecd6363bae62b25fc841a770e9c85ba
joecatarata <joecatarata@gmail.com>
2017-10-07 03:58:32 +0800

    Added headers file and other settings

--- Carver.py
+++ Carver.py
@@ -2,21 +2,37 @@ import threading
 import os
 import pickle
 
+def openDrive(driveLetter):
+    drive = open("\\\\.\\"+driveLetter+":", 'rb')
+    return drive
 def loadSettings():
     pass
 
-def SearchUsingTrailer():
+def SearchUsingTrailer(headers, choices):
     pass
 
 
 def main():
-    headers = {'jpgHeader': b'\xFF\xD8',
-               'jpgTrailer': b'\xFF\xD9',
-               'pdfHeader': b'\x25\x50',
-               'pdfTrailer': b'\x0A\x25\x25\x45\x4F\x46',
-               }
+    filename = 'headers'
+    file = open(filename, 'rb')
+    headers = pickle.load(file)
+    print(headers)
+    choices = []
 
+    while True:
+        print("Choose which file types will you want to recover")
+        print("[1] .jpg/.jpeg")
+        print("[2] .pdf")
+        print("[3] ..docx")
+        choice = int(input("Enter choice: "))
 
+        choices.append(choice)
+
+        AskMore = int(input("Do you want to input more files?[0/1]: "))
+        if AskMore == 1:
+            continue
+        else:
+            SearchUsingTrailer(headers, choices)
 
 
 if __name__ == "__main__":
