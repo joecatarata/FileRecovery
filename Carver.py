4b3af9fcc57274e01562041440259874bbeb35c3
joecatarata <joecatarata@gmail.com>
2017-10-07 02:55:38 +0800

    Carver file added

--- Carver.py
+++ Carver.py
@@ -5,8 +5,18 @@ import pickle
 def loadSettings():
     pass
 
+def SearchUsingTrailer():
+    pass
+
+
 def main():
-    settings = {}
+    headers = {'jpgHeader': b'\xFF\xD8',
+               'jpgTrailer': b'\xFF\xD9',
+               'pdfHeader': b'\x25\x50',
+               'pdfTrailer': b'\x0A\x25\x25\x45\x4F\x46',
+               }
+
+
 
 
 if __name__ == "__main__":
